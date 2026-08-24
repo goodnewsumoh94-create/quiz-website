1b5ce93516ce5b8b3fb8bc4b8a0bad19259fe50a
Goodnews <goodnewsumoh94@gmail.com>
2026-08-24 07:31:38 +0100

    ls

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -54,7 +54,7 @@ function App() {
   } else if (view === "study") {
     content = (
         <Study onBack={() => setView("quiz")}
-        onPracticeTopic={handlePracticeTopic()}
+        onPracticeTopic={handlePracticeTopic}
         />
     );
   } else {
